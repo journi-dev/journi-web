795c1c1e4139d41954b85d2a193cc4f37ba3cb4a
Jamal Riley <73957695+jamalmriley@users.noreply.github.com>
2023-06-11 22:08:41 -0500

    Update Menu component in Settings
    - Show MUI table of values from uploaded file.
    - Add checkbox and button.

--- src/pages/Settings/Menu.js
+++ src/pages/Settings/Menu.js
@@ -1,7 +1,201 @@
-import React from 'react'
+import { CloudUpload, FileUpload } from "@mui/icons-material";
+import {
+  Checkbox,
+  Container,
+  FormControlLabel,
+  FormGroup,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { useRef, useState } from "react";
+import { ExcelRenderer } from "react-excel-renderer";
+import { makeStyles } from "tss-react/mui";
+import { fillEmptyValues } from "../../utils/Helpers";
+import { CustomButton } from "../../components/ui/CustomComponents";
+
+const useStyles = makeStyles()((theme) => {
+  return {
+    form: {
+      width: "30%",
+    },
+    inputContainer: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      width: "100%",
+    },
+    dragActive: {
+      backgroundColor: "#ffffff",
+    },
+    dragFileElement: {
+      backgroundColor: "#ffff00",
+    },
+  };
+});
 
 export default function Menu() {
+  const { classes } = useStyles();
+  const [dragActive, setDragActive] = useState(false);
+  const [header, setHeader] = useState([]);
+  const [cols, setCols] = useState([]);
+  const [menuItemCount, setMenuItemCount] = useState(0);
+  const [overwrite, setOverwrite] = useState(false);
+  const inputRef = useRef(null);
+
+  const handleFile = (file) => {
+    ExcelRenderer(file, (err, response) => {
+      if (err) {
+        console.error(err);
+      } else {
+        let rows = response.rows.filter((row) => row.length > 1);
+        rows = fillEmptyValues(rows, "", rows[0].length);
+        setHeader(rows[0]);
+        setCols(rows);
+        setMenuItemCount(rows.length - 1);
+        console.log(rows);
+      }
+    });
+  };
+
+  const handleDrag = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (e.type === "dragenter" || e.type === "dragover") setDragActive(true);
+    else if (e.type === "dragleave") setDragActive(false);
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragActive(false);
+    if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+      handleFile(e.dataTransfer.files[0]);
+    }
+  };
+
+  /* const handleButtonClick = () => {
+    inputRef.current.click();
+  }; */
+
   return (
-    <div>Menu</div>
-  )
+    <div>
+      <Typography>Menu</Typography>
+
+      <form
+        className={classes.form}
+        onDragEnter={handleDrag}
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <input
+          ref={inputRef}
+          type="file"
+          id="input-file-upload"
+          multiple={false}
+          onChange={(e) => {
+            if (e.target.files && e.target.files[0]) {
+              handleFile(e.target.files[0]);
+            }
+          }}
+          hidden
+        />
+        <label htmlFor="input-file-upload">
+          <Container
+            className={`placeholder-light ${classes.inputContainer} ${
+              dragActive ? classes.dragActive : ""
+            }`}
+          >
+            <FileUpload fontSize="large" />
+            <Typography variant="h6">Drag and drop your file here</Typography>
+            <Typography className="click-here-text">
+              or click here to upload a file
+            </Typography>
+            <Typography variant="caption" display="block" sx={{ mt: 1 }}>
+              Accepted file types: .xlsx, .csv, .tsv
+            </Typography>
+          </Container>
+        </label>
+        {dragActive && (
+          <div
+            id="drag-file-element"
+            onDragEnter={handleDrag}
+            onDragLeave={handleDrag}
+            onDragOver={handleDrag}
+            onDrop={handleDrop}
+          ></div>
+        )}
+      </form>
+
+      {menuItemCount > 0 && (
+        <div>
+          <TableContainer component={Paper} sx={{ maxHeight: 400 }}>
+            <Table sx={{ minWidth: 650 }} stickyHeader>
+              <TableHead>
+                <TableRow>
+                  {header.map((h, i) => (
+                    <TableCell align="center" key={i}>
+                      {h}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {cols.slice(1).map((col, i) => (
+                  <TableRow key={i}>
+                    {col.map((c, i) => (
+                      <TableCell key={i}>{c}</TableCell>
+                    ))}
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+
+          <FormGroup>
+            <FormControlLabel
+              control={<Checkbox defaultChecked />}
+              label="Overwite current menu"
+              checked={overwrite}
+              onChange={(e) => setOverwrite(e.target.checked)}
+            />
+          </FormGroup>
+          <CustomButton variant="contained" startIcon={<CloudUpload />}>
+            Upload {menuItemCount} item{menuItemCount === 1 ? "" : "s"}
+          </CustomButton>
+        </div>
+      )}
+
+      {/* <table
+        style={{
+          borderCollapse: "collapse",
+          margin: "10px auto",
+          border: "1px auto black",
+        }}
+      >
+        <thead>
+          <tr>
+            {header.map((h, i) => (
+              <th style={{ border: "1px auto black" }} key={i}>
+                {h}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {cols.slice(1).map((col, i) => (
+            <tr key={i}>
+              {col.map((c, i) => (
+                <td key={i}>{c}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table> */}
+    </div>
+  );
 }
--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -1,17 +1,49 @@
-import React, { useState } from "react";
+// import { useState } from "react";
 import { TabTitle } from "../../utils/TabTitle";
-import { Box, Typography } from "@mui/material";
-import GiftCards from "./GiftCards";
+import { Box, Container, Grid, Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
-import { storage } from "../../utils/Firebase";
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+// import { storage } from "../../utils/Firebase";
+// import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import {
+  AccountCircle,
+  CardGiftcard,
+  CreditCard,
+  Devices,
+  DisplaySettings,
+  Help,
+  IntegrationInstructions,
+  MenuBook,
+  People,
+  Power,
+  Storefront,
+  TaskAlt,
+} from "@mui/icons-material";
+import { makeStyles } from "tss-react/mui";
+import Menu from "./Menu";
+
+const useStyles = makeStyles()((theme) => {
+  return {
+    menuItem: {
+      width: 250,
+      height: 125,
+      backgroundColor: theme.palette.tertiary.main,
+      color: "#fff",
+      borderRadius: 10,
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  };
+});
 
 export default function Settings() {
   TabTitle("settings");
   const { t } = useTranslation();
+  const { classes } = useStyles();
   // const imageListRef = ref(storage, "projectFiles");
 
-  const uploadFile = async () => {
+  /* const uploadFile = async () => {
     if (!fileUpload) return;
     const imageRef = ref(storage, `projectFiles/${fileUpload.name}`);
     try {
@@ -23,33 +55,100 @@ export default function Settings() {
     } catch (err) {
       console.error(err);
     }
-  };
+  }; */
 
-  // useEffect(() => {
-  //   listAll(imageListRef).then((res) => {
-  //     res.items.forEach((item) => {
-  //       getDownloadURL(item).then((url) => {
-  //         setImageList((prev) => [...prev, url]);
-  //       });
-  //     });
-  //   });
-  // });
+  /* useEffect(() => {
+    listAll(imageListRef).then((res) => {
+      res.items.forEach((item) => {
+        getDownloadURL(item).then((url) => {
+          setImageList((prev) => [...prev, url]);
+        });
+      });
+    });
+  }); */
 
-  const [fileUpload, setFileUpload] = useState(null);
-  const [imageList, setImageList] = useState([]);
+  // const [fileUpload, setFileUpload] = useState(null);
+  // const [imageList, setImageList] = useState([]);
+
+  const settingsMenu = [
+    {
+      text: "Business Info",
+      icon: <Storefront fontSize="large" color="itemButton" />,
+      path: "/business-info",
+    },
+    {
+      text: "Menu & Retail",
+      icon: <MenuBook fontSize="large" color="itemButton" />,
+      path: "/menu",
+    },
+    {
+      text: "Users & Patrons",
+      icon: <People fontSize="large" color="itemButton" />,
+      path: "/users",
+    },
+    {
+      text: "App & Website",
+      icon: <Devices fontSize="large" color="itemButton" />,
+      path: "/my-app",
+    },
+    {
+      text: "Integrations",
+      icon: <Power fontSize="large" color="itemButton" />,
+      path: "/integrations",
+    },
+    {
+      text: "Gift Cards",
+      icon: <CardGiftcard fontSize="large" color="itemButton" />,
+      path: "/gift-cards",
+    },
+    {
+      text: "Billing",
+      icon: <CreditCard fontSize="large" color="itemButton" />,
+      path: "/billing",
+    },
+    {
+      text: "Develop",
+      icon: <IntegrationInstructions fontSize="large" color="itemButton" />,
+      path: "/develop",
+    },
+    {
+      text: "Journi Settings",
+      icon: <DisplaySettings fontSize="large" color="itemButton" />,
+      path: "/platform-settings",
+    },
+
+    {
+      text: "Get Support",
+      icon: <Help fontSize="large" color="itemButton" />,
+      path: "/help",
+    },
+    {
+      text: "Task Management",
+      icon: <TaskAlt fontSize="large" color="itemButton" />,
+      path: "/audio",
+    },
+    {
+      text: "My Account",
+      icon: <AccountCircle fontSize="large" color="itemButton" />,
+      path: "/my-account",
+    },
+  ];
 
   return (
     <Box>
       <Typography variant="h5">{t("settings")}</Typography>
-      <GiftCards />
 
-      <div>
-        <input type="file" onChange={(e) => setFileUpload(e.target.files[0])} />
-        <button onClick={uploadFile}>Upload</button>
-        {imageList.map((url) => {
-          return <img src={url} alt="" />;
-        })}
-      </div>
+      <Menu />
+      <Grid container>
+        {settingsMenu.map((item) => (
+          <Grid item xs={12} sm={6} md={3}>
+            <Container className={classes.menuItem} sx={{ my: 2 }}>
+              {item.icon}
+              <Typography variant="h6">{item.text}</Typography>
+            </Container>
+          </Grid>
+        ))}
+      </Grid>
     </Box>
   );
 }
